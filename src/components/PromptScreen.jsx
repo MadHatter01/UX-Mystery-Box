2c70a1f52ed2b2472db4af037ffa1708b0bf9a87
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-11-19 19:32:08 -0800

    text styling

--- src/components/PromptScreen.jsx
+++ src/components/PromptScreen.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const PromptScreen = ({ prompt, generatePrompt, nextRound }) => {
     return (
         <div id="promptScreen" style={{marginBottom:'5rem'}}>
-            <h2>Your Prompt</h2>
+            <h2>Prompt</h2>
             <div id="promptBox">{prompt.product ? (<>
                 <p><strong>Product:</strong> {prompt.product}</p>
                 <p><strong>Audience:</strong> {prompt.audience}</p>
                 <p><strong>Twist:</strong> {prompt.twist}</p>
             </>): ("Click to get a prompt")}
             </div>
-            <button onClick={generatePrompt}>Get New Prompt</button>
+            <button class='customBtn' onClick={generatePrompt}>Get New Prompt</button>
             {/* <button onClick={nextRound}>View Constraints</button> */}
         </div>
     )
--- src/components/ViewConstraints.jsx
+++ src/components/ViewConstraints.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 const ViewConstraints = ({ constraint, generateConstraint }) => {
     return (
         <div id="viewConstraints" className="hidden">
-            <h2>Constraint Round</h2>
+            <h2>Constraint</h2>
             <div id="constraintBox">{constraint}</div>
-            <button className='customBtn' onClick={generateConstraint}>Get New Constraint</button>
+            <button class='customBtn' onClick={generateConstraint}>Get New Constraint</button>
         </div>
     )
 }
